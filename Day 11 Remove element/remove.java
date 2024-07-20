b15fefa7f612a603fe8d93a1b61598132f916530
JeetendraGarag <114762959+JeetendraGarag@users.noreply.github.com>
2024-07-20 19:02:02 +0530

    Add files via upload

---
+++ Day 11 Remove element/remove.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+     int n =  nums.length;
+     int p=0;
+     for(int i = 0;i < n;i++){
+        if(nums[i] != val){
+            nums[p]=nums[i];
+            p++;
+        }
+     }   return p;
+    }
+}
